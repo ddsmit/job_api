50fd67dd0dee05f278405ddf4c792ce035ed2c5b
David Smit <david.d.smit@gmail.com>
2020-09-19 01:47:59 -0400

    Adding env var

--- services/mongo/connection.py
+++ services/mongo/connection.py
@@ -1,11 +1,15 @@
 import pymongo
 import json
 import pathlib
+import os
 
-parameters_path = pathlib.Path('parameters', 'mongo.json')
+try:
+    parameters_path = pathlib.Path('parameters', 'mongo.json')
+    if not parameters_path.exists():
+        parameters_path = pathlib.Path('..', '..', 'parameters', 'mongo.json')
+    parameters = json.load(open(parameters_path))
+    connection = parameters['Connection']
+except:
+    connection = os.environ.get('Connection')
 
-if not parameters_path.exists():
-    parameters_path = pathlib.Path('..','..','parameters', 'mongo.json')
-
-parameters = json.load(open(parameters_path))
 client = pymongo.MongoClient(parameters['Connection'])
